118a3412a09126d889457ce95e42a2dece50cb23
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-10-29 19:00:07 +0100

    Added the Basic Structure for the Product

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+export type ProductDocument = mongoose.HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+    @Prop()
+    name: string;
+
+    @Prop()
+    price: number;
+
+    @Prop()
+    category: string;
+
+    @Prop()
+    quantity: number;
+
+    @Prop()
+    keywords: string;
+    
+}
