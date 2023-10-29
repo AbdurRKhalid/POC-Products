90abdb2f27577fbe2278662b821fdd186464c87b
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-10-29 20:19:30 +0100

    Successfully Exposed the Products Controller End Points

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from '../config/config';
+import { ProductsModule } from './products/products.module';
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb+srv://${config.database.DB_USERNAME}:${config.database.DB_PASSWORD}@cluster0.w4ykwfl.mongodb.net/?retryWrites=true&w=majority`)],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${config.database.DB_USERNAME}:${config.database.DB_PASSWORD}@cluster0.w4ykwfl.mongodb.net/?retryWrites=true&w=majority`), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
